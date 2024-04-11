26b62ecc6d10abc1c5ae4e2b5c92307a89af6bb4
Ordinarabbit <33993639+lyh983012@users.noreply.github.com>
2024-04-11 20:01:45 +0800

    Delete .ipynb_checkpoints directory

--- .ipynb_checkpoints/install-checkpoint.sh
+++
@@ -1,6 +0,0 @@
-python -m pip install pybind11
-echo "compile rdp_usb lib..."
-cd tianmoucv/rdp_usb/
-sh compile_pybind.sh
-cd ../..
-python -m pip install .
--- .ipynb_checkpoints/update-checkpoint.sh
+++
@@ -1,3 +0,0 @@
-python -m pip uninstall tianmoucv | echo "y" 
-python -m pip install .
-
