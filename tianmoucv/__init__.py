2c878843b0395c22db4f554f52e424d8b3685190
linyh20 <532109881@qq.com>
2025-03-19 21:32:00 +0800

    update

--- tianmoucv/__init__.py
+++ tianmoucv/__init__.py
@@ -13,5 +13,5 @@ __author__ = 'Y. Lin'
 __contributor__ = 'T. Wang, Y. Chen, Y. Li'
 __authorEmail__ = '532109881@qq.com'
 
-print('TianMouCV™ 0.3.8.8, via',__author__,' T. Wang & Y. Li add Denoise')
+print('TianMouCV™ 0.3.4.0, via',__author__,' update install logic and nature reconstructor')
 
